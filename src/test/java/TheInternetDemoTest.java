5152dc6ddf44c0432757ae1ac8cfaa39d26b9fad
rodikno <rodikno@gmail.com>
2024-08-20 13:45:47 +0200

    Add initial Selenium test setup
    Implemented basic Selenium test with a sample main page object and a test case for form submission. Added necessary dependencies, project configuration files, and a .gitignore for common IDEs and build directories.

---
+++ src/test/java/TheInternetDemoTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import page.objects.TheInternetMainPage;
+
+public class TheInternetDemoTest {
+
+    ChromeDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void hoversTest() throws InterruptedException {
+
+        //Opening the page
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.manage().window().fullscreen();
+
+        TheInternetMainPage page = new TheInternetMainPage(driver);
+        page.getTextInput().sendKeys("Hello");
+        page.submitForm();
+    }
+
+
+
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
